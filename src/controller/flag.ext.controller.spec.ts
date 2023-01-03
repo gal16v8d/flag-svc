5206f26c5e45accd19eee2e39e34014ce51267a3
alexg <alex.galvis.sistemas@gmail.com>
2023-01-03 18:56:23 -0500

    Add sonar props to scan this project

--- src/controller/flag.ext.controller.spec.ts
+++ src/controller/flag.ext.controller.spec.ts
@@ -2,13 +2,13 @@ import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import config from '../config/config';
 import { FlagService } from '../service/flag.service';
-import { flagArray, mockFlag1 } from '../__mocks__/flag.mock';
+import { mockFlag1 } from '../__mocks__/flag.mock';
 import { FlagExtController } from './flag.ext.controller';
 
 const APP_ID = '1';
 
 describe('FlagExt Controller test suite', () => {
-  let flagController: FlagExtController;
+  let controller: FlagExtController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -18,23 +18,17 @@ describe('FlagExt Controller test suite', () => {
         {
           provide: FlagService,
           useValue: {
-            create: jest.fn().mockResolvedValue(mockFlag1),
-            findAll: jest.fn().mockResolvedValue(flagArray),
-            findOne: jest.fn().mockResolvedValue(mockFlag1),
-            findByName: jest.fn().mockResolvedValue(mockFlag1),
             findByNameAndAppId: jest.fn().mockResolvedValue(mockFlag1),
-            update: jest.fn().mockResolvedValue(mockFlag1),
-            delete: jest.fn().mockResolvedValue(mockFlag1),
           },
         },
       ],
     }).compile();
 
-    flagController = app.get<FlagExtController>(FlagExtController);
+    controller = app.get<FlagExtController>(FlagExtController);
   });
 
   it('should return flag by name and id', async () => {
-    const app = await flagController.findByNameAndAppId(APP_ID, '1', false);
+    const app = await controller.findByNameAndAppId(APP_ID, '1', false);
     expect(app).toEqual(mockFlag1);
   });
 });
